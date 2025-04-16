6c14a46be86c489ec3d199b2b69870cd91f4c884
RomanKrohin <romankrohin126@gmail.com>
2025-04-16 19:28:26 +0300

    feat: user service init

---
+++ user-service/build.gradle.kts
@@ -0,0 +1,33 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "ru.polyZog"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+dependencies {
+    implementation(libs.ktor.server.kafka)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
---
+++ user-service/src/main/kotlin/Serialization.kt
@@ -0,0 +1,30 @@
+package ru.polyZog
+
+import io.github.flaxoos.ktor.server.plugins.kafka.Kafka
+import io.github.flaxoos.ktor.server.plugins.kafka.MessageTimestampType
+import io.github.flaxoos.ktor.server.plugins.kafka.TopicName
+import io.github.flaxoos.ktor.server.plugins.kafka.admin
+import io.github.flaxoos.ktor.server.plugins.kafka.common
+import io.github.flaxoos.ktor.server.plugins.kafka.consumer
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerConfig
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerRecordHandler
+import io.github.flaxoos.ktor.server.plugins.kafka.producer
+import io.github.flaxoos.ktor.server.plugins.kafka.registerSchemas
+import io.github.flaxoos.ktor.server.plugins.kafka.topic
+import io.ktor.client.HttpClient
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+    routing {
+        get("/json/kotlinx-serialization") {
+                call.respond(mapOf("hello" to "world"))
+            }
+    }
+}
