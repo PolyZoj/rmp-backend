ab800872e46b4d682fba67530a11d8dc8db3f73a
Роман Крохин <romankrohin@MacBook-Pro-Roman.local>
2025-05-07 04:09:53 +0300

    feat: new compose and minor fixes

---
+++ stats/bin/main/models/Models.kt
@@ -0,0 +1,32 @@
+package ru.polyZoj.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class StatsResponse(
+    val level: Int,
+    val xp: Int,
+    val calorie_count: Int,
+    val water_count: Int,
+    val workouts_count: Int,
+    val completed_challenges: Int
+)
+
+@Serializable
+data class AddStatsRequest(
+    val id: String,
+    val type: String,
+    val add: String
+)
+
+@Serializable
+data class DataPayload(
+    val message: String,
+    val params: List<String>
+)
+
+@Serializable
+data class StatusResponse(
+    val status: Boolean
+)
